e2f46110219f6cf5a5bfd6d80a1db1aadb19ed05
吴浩锐 <7692783+wuhaorui@user.noreply.gitee.com>
2024-07-07 00:13:45 +0800

    阿里云直传

--- config/aliyun.php
+++ config/aliyun.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+    'appKeyId' => env('ALIYUN_APP_KEY_ID', ''),
+    'appSecret' => env('ALIYUN_APP_KEY_SECRET', ''),
+    'bucket' => 'harry-online-com.oss-cn-guangzhou.aliyuncs.com',
+    'endpoint' => 'oss-cn-guangzhou.aliyuncs.com',
+];
